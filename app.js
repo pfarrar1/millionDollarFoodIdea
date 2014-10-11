b4b6ef4a8e97ba79c374a72fbf81757dddf473b5
Patrick Farrar <patrick@live-9.com>
2014-10-11 14:52:20 -0500

    inital views

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-app.js
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var millionDollarFoodIdea = angular.module('millionDollarFoodIdea', ['ngRoute'])
+
+
+millionDollarFoodIdea.config(['$routeProvider', function($routeProvider) {
+  
+  $routeProvider.when('/', {templateUrl: 'app/views/search.html'});
+  $routeProvider.when('/view', {templateUrl: 'app/views/restaurantProfile.html'});
+  $routeProvider.otherwise({redirectTo: '/'});
+
+
+}]);
--- app/applicationController.js
+++
@@ -1,1 +0,0 @@
-applicationController.js
