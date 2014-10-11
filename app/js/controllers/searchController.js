8b110cf6f3967f2d9c46b84aa584ea0008136d90
Patrick Farrar <patrick@live-9.com>
2014-10-11 18:28:02 -0500

    more work

--- app/js/controllers/searchController.js
+++ app/js/controllers/searchController.js
@@ -2,156 +2,125 @@ millionDollarFoodIdea.controller('searchController', ['$scope', '$http','dataSer
 	
 	$scope.greeting = "faskjhasdfjklh";
 
+	$scope.restaurant = '',
+
 	$scope.submitRestaurantSearch = function(restaurant) {
-		dataService.get(restaurant);
+		// dataService.get(restaurant);
+
+		var name = restaurant.name;
+		var zipcode = restaurant.zipCode;
+
+	// Simple GET request example :
+	$http.get('http://drinkndev.herokuapp.com/get-restaurant/?name='+name+'&zipcode='+zipcode).
+	  success(function(data, status, headers, config) {
+			alert('success');
+	    $scope.result = data;
+	  }).
+	  error(function(data, status, headers, config) {
+	    alert('failure');
+	  });
+
+
 	};
 
 
-	$scope.restaurants = [
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		}
-		,{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		}
-		,{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		}
-		,{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		},
-		{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		}
-		,{
-			name: 'Hot Dougs',	
-		},
-		{
-			name: 'Kumas',	
-		},
-		{
-			name: "Larry's"
-		}
-	]
+	// $scope.restaurants = [
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	},
+	// 	{
+	// 		name: 'Hot Dougs',	
+	// 	},
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	}
+	// 	,{
+	// 		name: 'Hot Dougs',	
+	// 	},
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	},
+	// 	{
+	// 		name: 'Hot Dougs',	
+	// 	},
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	},
+	// 	{
+	// 		name: 'Hot Dougs',	
+	// 	},
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	},
+	// 	{
+	// 		name: 'Hot Dougs',	
+	// 	},
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	}
+	// 	,{
+	// 		name: 'Hot Dougs',	
+	// 	},
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	},
+	// 	{
+	// 		name: 'Hot Dougs',	
+	// 	},
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	},
+	// 	{
+	// 		name: 'Hot Dougs',	
+	// 	},
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	},
+	// 	{
+	// 		name: 'Hot Dougs',	
+	// 	},
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	}
+	// 	,{
+	// 		name: 'Hot Dougs',	
+	// 	},
+	// 	{
+	// 		name: 'Kumas',	
+	// 	},
+	// 	{
+	// 		name: "Larry's"
+	// 	}
+	// ]
 	
 }]);
--- app/js/services/dataService.js
+++ app/js/services/dataService.js
@@ -1,13 +1,14 @@
 millionDollarFoodIdea.factory('dataService', ['$window','$http', function($http) {
-return {
+	
+	return {
 		get: getDataFromEndpoint
-	}
+	};
 
 	function getDataFromEndpoint(postObject) {
 		alert('Get data from endpoint in data service');
 		console.log(postObject);
 		var data = postObject;
-		
+
 		$http.post('/someUrl', data).
 		  success(function(data, status, headers, config) {
 		    $scope.restaurant = data;
